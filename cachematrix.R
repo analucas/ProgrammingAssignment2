9c3d299a5bf5e0be7101bf693c85128cf1ae7d45
analucas <ana@santoslucas.com>
2014-06-22 22:42:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 
-## This function creates a special "matrix" object that can cache its inverse
+#
+## This function creates a special "matrix" object that can cache its inverse.
+## If the original matrix is singular then the inverse is set to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,7 +11,12 @@ makeCacheMatrix <- function(x = matrix()) {
     i <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) i <<- solve
+  ## if determinant of x is zero than the matrix is singular
+  if(det(x)==0)
+    {
+   setinverse <-NULL}
+  else
+  {setinverse <- function(solve) i <<- solve}
   getinverse <- function() i
   list(set = set, get = get,
        setinverse = setinverse,
@@ -20,18 +27,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special "matrix" returned 
 ## by makeCacheMatrix above. If the inverse has already been calculated 
 ## (and the matrix has not changed), then the cachesolve should retrieve 
-## the inverse from the cache
+## the inverse from the cache. verse.
+## If the original matrix is singular then a message is written with that information
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
+  data <- x$get()
+  z<-det(data)
+  if (z==0)
+  {print ("the matrix is singular")}
+  else
+    
+  {i <- x$getinverse()
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
-  data <- x$get()
+ 
   i <- solve(data)
   x$setinverse(i)
  i
-  
+  }  
 }
+
